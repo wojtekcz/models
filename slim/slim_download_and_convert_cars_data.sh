bb1e7040c548751c80994d059be6642af90008df
Wojciech Czarnowski <wcz@jatar.com.pl>
2017-05-10 22:06:11 -0700

    Updated scripts for converting and training inception_v3 with car-reco3 dataset

--- slim/slim_download_and_convert_cars_data.sh
+++ slim/slim_download_and_convert_cars_data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DATA_DIR=/workspace/data/cars
+DATA_DIR=/Users/wcz/Beanflows/All_Beans/Machine_Learning/innovavant-Training-Car-Recognition-Model/choose-network-architecture/data/car-reco3
 
 python download_and_convert_data.py \
     --dataset_name=cars \
--- slim/slim_retrain_with_cars.sh
+++ slim/slim_retrain_with_cars.sh
@@ -2,13 +2,12 @@
 
 export TF_CPP_MIN_LOG_LEVEL=3
 
-DATASET_DIR=/workspace/data/cars
-TRAIN_DIR=/workspace/cars-models/inception_v3
-CHECKPOINT_PATH=/workspace/my_checkpoints/inception_v3.ckpt
+DATASET_DIR=/workspace/choose-network-architecture/data/car-reco3
+TRAIN_DIR=/workspace/choose-network-architecture/train_logs/car-reco3_2
+CHECKPOINT_PATH=/workspace/choose-network-architecture/checkpoints/inception_v3.ckpt
 DATASET_NAME=cars
 MODEL_NAME=inception_v3
 
-cd /workspace/models/slim
 python train_image_classifier.py \
     --train_dir=${TRAIN_DIR} \
     --dataset_dir=${DATASET_DIR} \
@@ -17,5 +16,4 @@ python train_image_classifier.py \
     --model_name=${MODEL_NAME} \
     --checkpoint_path=${CHECKPOINT_PATH} \
     --checkpoint_exclude_scopes=InceptionV3/Logits,InceptionV3/AuxLogits \
-    --trainable_scopes=InceptionV3/Logits,InceptionV3/AuxLogits \
-    --save_summaries_secs=30
+    --trainable_scopes=InceptionV3/Logits,InceptionV3/AuxLogits
