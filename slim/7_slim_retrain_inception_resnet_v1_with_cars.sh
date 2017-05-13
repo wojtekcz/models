3cc6f70627f181b0457d72ce2d56d3a1e4d3ec81
Wojciech Czarnowski <wcz@jatar.com.pl>
2017-05-13 02:31:14 -0700

    Configs for first slew of trainings

---
+++ slim/7_slim_retrain_inception_resnet_v1_with_cars.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+export TF_CPP_MIN_LOG_LEVEL=3
+
+DATASET_DIR=/workspace/data/car-reco3
+TRAIN_DIR=/workspace/train_logs/7_car-reco3_resnet_v2
+CHECKPOINT_PATH=/workspace/checkpoints/resnet_v1_152.ckpt
+DATASET_NAME=cars
+MODEL_NAME=resnet_v1_152
+
+python train_image_classifier.py \
+    --train_dir=${TRAIN_DIR} \
+    --dataset_dir=${DATASET_DIR} \
+    --dataset_name=${DATASET_NAME} \
+    --dataset_split_name=train \
+    --model_name=${MODEL_NAME} \
+    --checkpoint_path=${CHECKPOINT_PATH} \
+    --checkpoint_exclude_scopes=resnet_v1_152/logits \
+    --trainable_scopes=resnet_v1_152/logits \
+    --save_summaries_secs=60 \
+    --log_every_n_steps=100
+#  --batch_size=32 \
