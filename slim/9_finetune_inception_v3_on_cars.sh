dd6fd687556cea498bb5516be2c5e707f4daa8fd
Wojciech Czarnowski <wcz@jatar.com.pl>
2017-05-13 09:05:00 -0700

    Added training 9 script

---
+++ slim/9_finetune_inception_v3_on_cars.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+export TF_CPP_MIN_LOG_LEVEL=3
+
+DATASET_DIR=/workspace/data/car-reco3-70
+TRAIN_DIR=/workspace/train_logs/9_car-reco3-70_inception_v3
+CHECKPOINT_PATH=/workspace/checkpoints/inception_v3.ckpt
+DATASET_NAME=cars
+MODEL_NAME=inception_v3
+
+python train_image_classifier.py \
+    --train_dir=${TRAIN_DIR} \
+    --dataset_dir=${DATASET_DIR} \
+    --dataset_name=${DATASET_NAME} \
+    --dataset_split_name=train \
+    --model_name=${MODEL_NAME} \
+    --checkpoint_path=${CHECKPOINT_PATH} \
+    --checkpoint_exclude_scopes=InceptionV3/Logits,InceptionV3/AuxLogits \
+    --trainable_scopes=InceptionV3/Logits,InceptionV3/AuxLogits \
+    --save_summaries_secs=60 \
+    --log_every_n_steps=100
