448797e1b5bc125a28d394063edf1f72b8ffcb32
Wojciech Czarnowski <wcz@jatar.com.pl>
2017-05-16 08:52:04 -0700

    Added training 15 script

---
+++ slim/scripts/15_train_resnet_v2_152.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+export TF_CPP_MIN_LOG_LEVEL=3
+
+DATASET_DIR=/workspace/data/car-reco3-70
+TRAIN_DIR=/workspace/train_logs/15_car-reco3-70_resnet_v2_152
+DATASET_NAME=cars
+MODEL_NAME=resnet_v2_152
+
+python train_image_classifier.py \
+    --train_dir=${TRAIN_DIR} \
+    --dataset_dir=${DATASET_DIR} \
+    --dataset_name=${DATASET_NAME} \
+    --dataset_split_name=train \
+    --model_name=${MODEL_NAME} \
+    --save_summaries_secs=60 \
+    --log_every_n_steps=100
